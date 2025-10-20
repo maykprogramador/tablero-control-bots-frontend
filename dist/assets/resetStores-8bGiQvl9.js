a7f68bea65bb39bd4b05f43d97a0f13f8d077e4e
MAYKOL STIVEEN PLAZA CASTRILLON <maykol.plazac@zentria.com.co>
2025-10-20 09:14:08 -0500

    npm run build

--- dist/assets/resetStores-8bGiQvl9.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/resetStores-8bGiQvl9.js
-import{p as e}from"./index-D4lNAmAA.js";function r(){for(const t of e._s.values())typeof t.resetState=="function"&&t.resetState()}export{r as resetAllStores};
-========
-import{p as e}from"./index-CQCKuxSs.js";function r(){for(const t of e._s.values())typeof t.resetState=="function"&&t.resetState()}export{r as resetAllStores};
->>>>>>>> 7df2913f8ff95a7196cbf29cf160b134616eee5d:dist/assets/resetStores-DcQwV8dw.js
